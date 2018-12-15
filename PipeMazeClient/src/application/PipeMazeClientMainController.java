303d339341c0a8ad914ddd33d7ee7c26059db2c6
einhorneyal <eyal1008@gmail.com>
2018-12-15 18:52:29 +0200

    Revert "Merge branch 'master' of https://github.com/essale/patam_ppipe_game"
    This reverts commit 0013c85be102ec35f5be9a3d19ce773be8476014, reversing
    changes made to 6d7b5cf8dc7463fb0eefc4f6fb9e30a504f640ee.

--- PipeMazeClient/src/application/PipeMazeClientMainController.java
+++ PipeMazeClient/src/application/PipeMazeClientMainController.java
@@ -4,19 +4,16 @@ import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import View.Theme;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
-import utils.Rotater;
 
 public class PipeMazeClientMainController implements Initializable {
 	
-		private Theme theme = new Theme("pickleRick");
-		private char[][] defaultPipeGame = 
+	
+
+
+		char[][] defaultPipeGame = 
 			{
 					{'s','-','|','L'},
 					{'L','-','|','L'},
@@ -30,19 +27,9 @@ public class PipeMazeClientMainController implements Initializable {
 
 		public void setPipeGameCanvas(char[][] lvl) 
 		{
-			pgc.cleanGame();
-			pgc.setLevel(lvl,theme);	
-			pgc.setOnMouseClicked(new EventHandler<Event>() {
-
-				@Override
-				public void handle(Event event) {
-					MouseEvent me = (MouseEvent) event;
-					double w = pgc.getWidth() / lvl[0].length; 
-					double h = pgc.getHeight() / lvl.length; 
-					defaultPipeGame = Rotater.rotate(lvl,(int)Math.floor(me.getY()/h),(int)Math.floor(me.getX()/w));
-					setPipeGameCanvas(defaultPipeGame);
-				}
-			});
+			pgc.scaleXProperty();
+			pgc.scaleYProperty();
+			pgc.setLevel(lvl);	
 		}
 		
 		@Override
