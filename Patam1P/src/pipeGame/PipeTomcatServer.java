a7ecc37a2f5b3b0ef7661abc7ea09b4b5246a84f
essaleitay <itayess10@gmail.com>
2018-12-15 19:04:03 +0200

    push again

---
+++ Patam1P/src/pipeGame/PipeTomcatServer.java
@@ -0,0 +1,109 @@
+package pipeGame;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import server.ClientHandler;
+import server.Server;
+
+public class PipeTomcatServer implements Server {
+    static int TCP_SERVER_PORT = 32;
+    private ServerSocket serverSocket;
+    private boolean stop = false;
+	private int counter = 0;
+	
+	
+	
+    public PipeTomcatServer(int port) {
+    	this.TCP_SERVER_PORT = port;
+    	 
+    }
+    public PipeTomcatServer() {
+    	this.TCP_SERVER_PORT = 32;
+
+    }
+    
+    
+    @Override
+    public void start(ClientHandler clientHandler){
+    	new Thread(() -> {
+            try {
+                startServer(clientHandler);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    
+	}
+    
+    private void startServer(ClientHandler clientHandler) throws IOException {
+        serverSocket = new ServerSocket(PipeTomcatServer.TCP_SERVER_PORT);
+        serverSocket.setSoTimeout(1000);
+        //System.out.println("Server connected - waiting");
+        while (!stop) {
+            try {
+                Socket aClient = serverSocket.accept();
+                ExecutorService threadpool = Executors.newFixedThreadPool(10);
+                ThreadPoolExecutor tpe =  new ThreadPoolExecutor(1, 5, 10, TimeUnit.SECONDS,
+            			new PriorityBlockingQueue<Runnable>());
+                if(aClient != null) 
+                {
+                    
+					tpe.execute(new PipePriorityRunnable(aClient.getInputStream().available()) {
+						@Override
+						public void run() {
+							try {
+				                updateCounter();
+			                	System.out.println(counter);
+			                	clientHandler.handle(aClient.getInputStream(), aClient.getOutputStream());
+								aClient.close();
+								
+				            } catch (IOException e) {
+				            	e.printStackTrace();
+				            } 
+						}
+					});
+                }
+                
+                
+                
+            } catch (SocketTimeoutException e) {
+//                //System.out.println("Client did not connect...");
+            }
+        }
+        serverSocket.close();
+    }
+
+	@Override
+	public synchronized void stop() {
+		stop=true;
+	}
+
+	public synchronized void updateCounter() 
+	{
+		this.counter++;
+	}
+	
+	public static void main(String[] args)
+	{
+		Server s = new PipeTomcatServer();
+		s.start(new PipeSimpleClientHandler());
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        s.stop();
+        //System.out.println("Closed server");
+	}
+}
