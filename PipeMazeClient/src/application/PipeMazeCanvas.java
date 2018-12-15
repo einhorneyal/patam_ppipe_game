9bdf40847149f08967086867dfa9673eed1c224c
essaleitay <itayess10@gmail.com>
2018-12-15 19:08:12 +0200

    client OK

--- PipeMazeClient/src/application/PipeMazeCanvas.java
+++ PipeMazeClient/src/application/PipeMazeCanvas.java
@@ -2,6 +2,8 @@ package application;
 
 import java.io.FileInputStream;
 
+import View.Theme;
+import javafx.beans.InvalidationListener;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
@@ -10,17 +12,60 @@ public class PipeMazeCanvas extends Canvas {
 
 
 	char[][] level ;
+	private Theme theme;
 	
 	
-	public void setLevel(char[][] lvl) 
+	public void setLevel(char[][] lvl, Theme theme) 
 	{
 		this.level = lvl;
-		System.out.println(lvl.length);
+		this.theme = theme;
 		redraw();
 	}
 	
-	public void redraw() 
-	{
+	@Override
+	public boolean isResizable() {
+		return true;
+	}
+
+	@Override
+	public double minHeight(double width) {
+		return 64;
+	}
+
+	@Override
+	public double maxHeight(double width) {
+		return 1000;
+	}
+
+	@Override
+	public double prefHeight(double width) {
+		return minHeight(width);
+	}
+
+	@Override
+	public double minWidth(double height) {
+		return 0;
+	}
+
+	@Override
+	public double maxWidth(double height) {
+		return 10000;
+	}
+
+	@Override
+	public void resize(double width, double height) {
+		super.setWidth(width);
+		super.setHeight(height);
+		this.cleanGame();
+		this.redraw();
+	}
+	
+	public void cleanGame() {
+		GraphicsContext gc = getGraphicsContext2D();
+		gc.clearRect(0, 0, getWidth(), getHeight());
+	}
+	
+	public void redraw() {
 		if(level != null) 
 		{
 			double W = getWidth();
@@ -29,87 +74,53 @@ public class PipeMazeCanvas extends Canvas {
 			double h = H / level.length; // real width
 			GraphicsContext gc = getGraphicsContext2D();
 
-			Image straightLine = null;
-			Image end  = null;
-			Image start  = null;
-			Image vertLine  = null;
-			Image verJ  = null;
-			Image verL  = null;
-			Image verF  = null;
-			Image ver7  = null;
-			Image background  = null;
-
-			try{
-				
-			//	straightLine = new Image(getClass().getResource( "/resorces/þþstraight.png" ).openStream());
-				//start = new Image(getClass().getResource( "/resorces/þþstartEnd.png" ).openStream());
-				//space = new Image(getClass().getResource( "/resorces/þþspace2.png" ).openStream());
-				//end = new Image(getClass().getResource( "./src/Resources/þþend.png" ).openStream());
-				//vertL = new Image(getClass().getResource( "./src/Resources/þþver.png" ).openStream());
-				straightLine = new Image(new FileInputStream("./src/Resources/pipe1.png"));
-				vertLine = new Image(new FileInputStream("./src/Resources/pipe2.png"));
-				start = new Image(new FileInputStream("./src/Resources/StartPickleRick.png"));
-				end = new Image(new FileInputStream("./src/Resources/Rat.png"));
-				verJ = new Image(new FileInputStream("./src/Resources/pipeJ.png"));
-				verF = new Image(new FileInputStream("./src/Resources/pipeF.png"));
-				ver7 = new Image(new FileInputStream("./src/Resources/pipe7.png"));
-				verL = new Image(new FileInputStream("./src/Resources/pipeL.png"));
-				background =   new Image(new FileInputStream("./src/Resources/Background.jpg"));
-				
-
-			}catch(Exception e) 
-			{
-				e.printStackTrace();
-			}
-
-			
-			gc.drawImage(background, W, H);
+			gc.drawImage(theme.getBackground(), W, H);
 			
 			for(int i = 0; i < level.length;i++)
 				for(int j = 0; j < level[0].length;j++) 
 				{
 					if(level[i][j] == '-') 
 					{
-						gc.drawImage(vertLine, j*w , i*h , w, h);
+						gc.drawImage(theme.getVertLine(), j*w , i*h , w, h);
 					    
 					}
 					else if(level[i][j] == '|') 
 					{
-						gc.drawImage(straightLine, j*w , i*h , w, h);
+						gc.drawImage(theme.getStraightLine(), j*w , i*h , w, h);
 						
 					}
 					else if(level[i][j] == 'L') 
 					{
-						gc.drawImage(verL, j*w , i*h , w, h);
+						gc.drawImage(theme.getVerL(), j*w , i*h , w, h);
 					}
 					else if(level[i][j] == 'F') 
 					{
-						gc.drawImage(verF, j*w , i*h , w, h);
+						gc.drawImage(theme.getVerF(), j*w , i*h , w, h);
 						
 
 					}
 					else if(level[i][j] == '7') 
 					{
-						gc.drawImage(ver7, j*w , i*h , w, h);
+						gc.drawImage(theme.getVer7(), j*w , i*h , w, h);
 
 					}
 					else if(level[i][j] == 'J') 
 					{
-						gc.drawImage(verJ, j*w , i*h , w, h);
+						gc.drawImage(theme.getVerJ(), j*w , i*h , w, h);
 
 
 					}
 					else if(level[i][j] == 's') 
 					{
-						gc.drawImage(start, j*w , i*h , w, h);
+						gc.drawImage(theme.getStart(), j*w , i*h , w, h);
 					}
 					else if(level[i][j] == 'g') 
 					{
-						gc.drawImage(end, j*w , i*h , w, h);
+						gc.drawImage(theme.getEnd(), j*w , i*h , w, h);
+						
 					}
 			
-
-				}
+			}
 		}
 	}
 }
